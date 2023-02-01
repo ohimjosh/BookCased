5069eb8bf67cf42cc0a4a474a8cd6e90751488f0
ohimjosh <joshmiranda.exe@gmail.com>
2023-02-01 17:16:56 -0500

    simple login

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function home() {
     return (
         <div>
-            <h1 class="text-3xl font-bold underline">Home</h1>
+            <h1 className="text-3xl font-bold underline">Home</h1>
         </div>
     );
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React from 'react'
 
 function login() {
-    return (
-        <div>
-            <h1 class="text-3xl font-bold underline">Login</h1>
-        </div>
-    );
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+      <div className='hidden sm:block'>
+
+      </div>
+      <div className='bg-gray-100 flex flex-col justify-center'>
+        <form className='max-w-[400px] w-full mx-auto bg-white p-4'>
+          <h2 className='text-4xl font-bold text-center py-6'>BookCased</h2>
+          <div className='flex flex-col py-2'>
+            <label htmlFor="">Username</label>
+            <input className='border p-2' type="text" />
+          </div>
+          <div className='flex flex-col py-2'>
+            <label htmlFor="">Password</label>
+            <input className='border p-2' type="password" />
+          </div>
+          <button className='border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white'>Sign In</button>
+          <div className='flex justify-between'>
+            <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
+            <p>Create an account</p>
+          </div>
+        </form>
+
+      </div>
+
+    </div>
+  )
 }
 
-export default login;
+export default login
