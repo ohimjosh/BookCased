53e0339ef12421d349d567aae46d6a56d9b67cf7
ohimjosh <joshmiranda.exe@gmail.com>
2023-02-01 14:54:39 -0500

    login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import About from './pages/about'
 import Profile from './pages/profile'
+import Login from './pages/login'
 import Error from './pages/error'
 
 function App() {
@@ -13,8 +14,8 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/*' element={<Error />} />
-
       </Routes>
     </Router>
   )
